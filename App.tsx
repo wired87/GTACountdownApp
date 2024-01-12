159544fe3c5759e03c2069b36d5642397374df38
wired <wired67@outlook.de>
2024-01-12 23:00:06 +0100

    bug hunter 666

--- App.tsx
+++ App.tsx
@@ -1,116 +1,221 @@
-import React, {useEffect, useState} from "react";
-import {ActivityIndicator, Dimensions, Platform, StyleSheet, Text, useWindowDimensions, View} from "react-native";
+import React, {useCallback, useEffect, useRef, useState} from "react";
+import {
+  ActivityIndicator,
+  Dimensions,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View
+} from "react-native";
 import * as Font from "expo-font";
 import CustomCountdown from "./src/CustomCountdown";
-import { ImageBackground } from "react-native";
 import * as SplashScreen from 'expo-splash-screen';
 
 const windowWidth = Dimensions.get('window').width;
 
-interface AppProps {}
+
+interface TimeRemaining {
+  years: number;
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
+
 
 // GOOGLE ADMOB
 import {BannerAd, BannerAdSize, TestIds} from 'react-native-google-mobile-ads';
 
-const adUnitIdBannerAd = __DEV__
+const adUnitIdBannerAdHeader = __DEV__
   ? TestIds.BANNER
   : Platform.OS === "ios" ?
-    "ca-app-pub-2225753085204049/2862976257" :
-    "ca-app-pub-2225753085204049/8777981057"
-/*
-<BannerAd
-unitId={adUnitIdBannerAd}
-size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
-requestOptions={{
-requestNonPersonalizedAdsOnly: true,
-}}
-/>*/
-const App : React.FC<AppProps> = () => {
+    "ca-app-pub-2225753085204049/9660357854" :
+    "ca-app-pub-2225753085204049/6545425146"
+
+
+const adUnitIdBannerAdFooter = __DEV__
+  ? TestIds.BANNER
+  : Platform.OS === "ios" ?
+    "ca-app-pub-2225753085204049/5721112841" :
+    "ca-app-pub-2225753085204049/1293098467"
+
+
+const App : React.FC = () => {
   
   const [appLoaded, setAppLoaded] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState<TimeRemaining | null>(
+    null as TimeRemaining
+  );
+
+  const bannerRefHeader = useRef(null);
+  const bannerRefFooter = useRef(null);
+
 
   useEffect(() => {
     async function prepare() {
       try {
-        // Keep the splash screen visible while fetching fonts
         await SplashScreen.preventAutoHideAsync();
 
-        // Load your fonts
         await Font.loadAsync({
           GTA: require("./assets/fonts/pricedow.ttf"),
         });
 
-        // Fonts are loaded, we can set appLoaded to true
         setAppLoaded(true);
       } catch (e) {
         console.warn(e);
       } finally {
-        // Hide the splash screen
         await SplashScreen.hideAsync();
       }
     }
 
-    prepare().then(r => console.log("Fonts have been successfully loaded!"));
+    prepare()
+      .then(() => console.log("Fonts successfully loaded"));
   }, []);
 
-  if (!appLoaded) {
-    return <ActivityIndicator size={20} />;
+  useEffect(() => {
+    if (appLoaded) {
+      setTimeRemaining(calculateTimeRemaining());
+    }
+  }, [appLoaded]);
+
+
+  const timer = useCallback(() => {
+    if (appLoaded && timeRemaining) {
+      return(
+        <CustomCountdown timeRemaining={timeRemaining} />
+      )
+    }else {
+      return(
+        <ActivityIndicator size={"large"} color={"white"} />
+      );
+
+    }
+  }, [appLoaded, timeRemaining]);
+
+
+  const targetDate = new Date("2025-01-04T00:00:00-00:00").getTime();
+
+  function calculateTimeRemaining() {
+    const now = new Date().getTime();
+    const difference = targetDate - now;
+
+    if (difference > 0) {
+      const c_days = Math.floor(difference / (1000 * 60 * 60 * 24));
+      let years = 0;
+      if (c_days > 365) {
+        years = Math.floor(c_days / 365);
+      }
+
+      const days = Math.floor((difference / (1000 * 60 * 60 * 24)) % 365);
+
+      const hours = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+      setTimeRemaining(
+        { years, days, hours, minutes, seconds }
+      )
+
+    } else {
+      return { years: 0, days: 0, hours: 0, minutes: 0, seconds: 0 };
+    }
   }
 
-  // Calculate the target date for December 25, 2024
-  const targetDate = new Date("2023-12-25T00:00:00").getTime();
+
+  useEffect(() => {
+    if (appLoaded) {
+      const interval = setInterval(() => {
+        calculateTimeRemaining();
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+
+  }, [timeRemaining, appLoaded]);
+
 
   return (
-    <View
+    <SafeAreaView
       style={styles.container}>
+
       <View style={styles.topBanner}>
         <BannerAd
-          unitId={adUnitIdBannerAd}
+          unitId={adUnitIdBannerAdHeader}
           size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+          onAdFailedToLoad={() => {
+            bannerRefHeader.current?.loadAd();
+          }}
           requestOptions={{
-            requestNonPersonalizedAdsOnly: true,
-          }}/>
+            requestNonPersonalizedAdsOnly: false,
+          }}
+        />
       </View>
-        <Text style={styles.title}>Trailer Release</Text>
-        <CustomCountdown targetDate={targetDate} />
+
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Release Countdown</Text>
+      </View>
+
+      {timer()}
+
       <View style={styles.bottomBanner}>
         <BannerAd
-          unitId={adUnitIdBannerAd}
+          unitId={adUnitIdBannerAdFooter}
           size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+          onAdFailedToLoad={() => {
+            bannerRefFooter.current?.loadAd();
+          }}
           requestOptions={{
-            requestNonPersonalizedAdsOnly: true,
+            requestNonPersonalizedAdsOnly: false,
           }}/>
       </View>
-    </View>
+
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
- 
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#1b1b1b",
-    resizeMode:'contain'
+    flexDirection: "column"
   },
-  title: {
-    fontSize: 50,
-    color: "white",
+  titleContainer: {
+    width: windowWidth,
+    paddingVertical: 40,
+    zIndex: 49,
+    flexDirection: "column",
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  titleNumber: {
+    fontSize: 450,
+    color: "orange",
     marginBottom: 20,
     fontFamily: "GTA",
+    position: "absolute",
+  },
+  title: {
+    zIndex: 50,
+    fontSize: 50,
+    color: "white",
+    fontFamily: "GTA",
+    textAlign: "center"
   },
   bottomBanner: {
     position: "absolute",
     bottom: 0,
     width: windowWidth,
-    height: 100,
+    paddingTop: Platform.OS === "ios" ? 20 : 0,
   },
   topBanner: {
+    top: 20,
     position: "absolute",
-    top: 0,
     width: windowWidth,
-    height: 100,
   }
 });
 
--- src/CustomCountdown.tsx
+++ src/CustomCountdown.tsx
@@ -1,107 +1,66 @@
-import React, { useEffect, useRef, useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import {View, Text, StyleSheet, Dimensions, FlatList, ActivityIndicator} from "react-native";
 
 interface CountdownProps {
-  targetDate: number;
+  timeRemaining: any;
 }
 
-interface TimeRemaining {
-  years: number;
-  days: number;
-  hours: number;
-  minutes: number;
-  seconds: number;
-}
 
-const CustomCountdown: React.FC<CountdownProps> = ({ targetDate }) => {
-  const [timeRemaining, setTimeRemaining] = useState<TimeRemaining>(
-    calculateTimeRemaining()
-  );
- 
- 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const remainingTime = calculateTimeRemaining();
-      setTimeRemaining(remainingTime);
 
-      if (
-        remainingTime.years === 0 &&
-        remainingTime.days === 0 &&
-        remainingTime.hours === 0 &&
-        remainingTime.minutes === 0 &&
-        remainingTime.seconds === 0
-      ) {
-       
-        clearInterval(interval);
-      }
-    }, 1000);
 
-    return () => clearInterval(interval);
-  }, []);
 
-  function calculateTimeRemaining() {
-    const now = new Date().getTime();
-    const difference = targetDate - now;
-
-    if (difference > 0) {
-      const c_days = Math.floor(difference / (1000 * 60 * 60 * 24));
-      var years = 0;
-      if (c_days > 365) {
-        years = Math.floor(c_days / 365);
-      }
-      const days = Math.floor((difference / (1000 * 60 * 60 * 24)) % 365);
-
-      const hours = Math.floor(
-        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
-      const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-
-      return { years, days, hours, minutes, seconds };
-    } else {
-      return { years: 0, days: 0, hours: 0, minutes: 0, seconds: 0 };
-    }
+const CustomCountdown: React.FC<CountdownProps> = (
+  {
+    timeRemaining,
   }
+) => {
 
-  return (
+  const renderItem = ({ item }) => (
+    <View style={styles.unitContainer}>
+      <Text style={styles.label}>{item[0].toUpperCase()}</Text>
+      <Text style={styles.value}>{item[1]}</Text>
+    </View>
+  );
+
+  return(
     <View style={styles.container}>
-      {Object.entries(timeRemaining).map(([unit, value]) => (
-        <View key={unit} style={styles.unitContainer}>
-          <Text style={styles.label}>{unit.toUpperCase()}</Text>
-          <Text style={styles.value}>{value}</Text>
-        </View>
-      ))}
-
+      <FlatList
+        horizontal
+        data={Object.entries(timeRemaining)}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={renderItem}
+      />
     </View>
   );
 };
 
+const windowWidth = Dimensions.get('window').width;
+
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center",
+    width: windowWidth,
+    justifyContent: "center",
     marginTop: 20,
+    alignItems: "center",
+    paddingVertical: 0,
   },
+
   unitContainer: {
+    marginHorizontal: 10,
     alignItems: "center",
     justifyContent: "center",
-    // backgroundColor:'pink',
-    height: 100,
-    width: 75,
-    marginHorizontal: 5,
   },
   label: {
     fontSize: 20,
     fontFamily: "GTA",
     color: "white",
-    marginBottom: 10,
+    textAlign: "center"
   },
   value: {
+    textAlign: "center",
     fontSize: 50,
     fontFamily: "GTA",
     color: "white",
-    elevation: 50,
   },
 });
 
